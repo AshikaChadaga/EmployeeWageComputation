ae0c86597f9823811c295075e8788001172f59b0
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-26 11:08:27 +0530

    REFACTORED: UC3 Part-Time Employee

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -6,14 +6,16 @@ public class EmployeeWageComputation {
 		
 		System.out.println("----- Welcome to Employee Wage Computation! -----");
 		
-		int IS_PRESENT = 1;
+		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_WAGE_PER_HOUR = 20;
 		
 		int empHrs, empWage;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if(empCheck == IS_PRESENT){
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if(empCheck == IS_FULL_TIME)
 			empHrs = 8;
-		}
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 4;
 		else
 			empHrs = 0;
 		
