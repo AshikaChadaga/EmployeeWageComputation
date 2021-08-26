49339b59fbf91b59424793fbc3f86a2ed3e95e8f
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-26 11:05:35 +0530

    REFACTORED: UC2 Daily Wages Calculation

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -6,13 +6,20 @@ public class EmployeeWageComputation {
 		
 		System.out.println("----- Welcome to Employee Wage Computation! -----");
 		
-		int empPresesent = 1;
+		int IS_PRESENT = 1;
+		int EMP_WAGE_PER_HOUR = 20;
+		
+		int empHrs, empWage;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if(empCheck == empPresesent){
-			System.out.println("Employee is Present");
+		if(empCheck == IS_PRESENT){
+			empHrs = 8;
 		}
 		else
-			System.out.println("Employee is Absent");
+			empHrs = 0;
+		
+		empWage = empHrs * EMP_WAGE_PER_HOUR;
+		System.out.println("Emp Hours : "+empHrs);
+		System.out.println("Emp Wage : "+empWage);
 	}
 
 }
