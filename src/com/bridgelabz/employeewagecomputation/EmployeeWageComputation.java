0c76f6df082cdfbc5ebe029f7feb76a22c912528
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-27 10:22:29 +0530

    REFACTORED: UC9 Saving Total Wage by Company

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -4,29 +4,30 @@ public class EmployeeWageComputation {
 
 	private static final int IS_FULL_TIME = 1;
 	private static final int IS_PART_TIME = 2;
-	
+
 	private final String company;
 	private final int empRate;
 	private final int numOFDays;
 	private final int maxHours;
-	
+	private int empWagePerMonth;
+
 	public EmployeeWageComputation(String company, int empRate, int numOFDays, int maxHours) {
 		this.company = company;
 		this.empRate = empRate;
 		this.numOFDays = numOFDays;
 		this.maxHours = maxHours;
 	}
-	public int calculateEmpWageForCompany() {
-		
-		System.out.println("----- Welcome to Employee Wage Computation! -----");
-		int empHrs=0, empWagePerMonth = 0, totalWorkingdays=0, totalEmpHours = 0;
-				
-		while(totalEmpHours <= this.maxHours && totalWorkingdays < this.numOFDays) {
-			
-			totalWorkingdays ++;
-			int empType = (int)Math.floor(Math.random() * 10) % 3;
-			
-			switch(empType) {
+
+	public void calculateEmpWageForCompany() {
+
+		int empHrs = 0, totalWorkingdays = 0, totalEmpHours = 0;
+
+		while (totalEmpHours <= maxHours && totalWorkingdays < numOFDays) {
+
+			totalWorkingdays++;
+			int empType = (int) Math.floor(Math.random() * 10) % 3;
+
+			switch (empType) {
 			case IS_FULL_TIME:
 				empHrs = 8;
 				break;
@@ -36,25 +37,33 @@ public class EmployeeWageComputation {
 			default:
 				empHrs = 0;
 			}
-			
+
 			totalEmpHours += empHrs;
-			int empWagePerDay = empHrs * this.empRate;
+			int empWagePerDay = empHrs * empRate;
 			empWagePerMonth += empWagePerDay;
-			System.out.println("Emp Wage Per Day : "+empWagePerDay);
+			System.out.println("Emp Wage Per Day : " + empWagePerDay);
 		}
-		
-		System.out.println("Emp Wage Per Month for Company: "+this.company+" is : "+empWagePerMonth);
-		return empWagePerMonth;
+
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Emp Wage Per Month for Company " + company + " is : " + empWagePerMonth;
+	}
+
 	public static void main(String[] args) {
+		
+		System.out.println("----- Welcome to Employee Wage Computation Program! -----");
 		EmployeeWageComputation bridgelabz = new EmployeeWageComputation("BridgeLabz", 20, 2, 10);
 		EmployeeWageComputation dMart = new EmployeeWageComputation("DMART", 40, 5, 20);
+
 		bridgelabz.calculateEmpWageForCompany();
+		System.out.println(bridgelabz);
 		System.out.println();
+
 		dMart.calculateEmpWageForCompany();
-		
-	
+		System.out.println(dMart);
+
 	}
 
 }
